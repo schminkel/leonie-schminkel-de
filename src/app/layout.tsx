50bbffedb20ab295bbf62007d44be635e4c4966f
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-03-04 09:20:57 +0100

    add favicon setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/png"
+          sizes="32x32"
+        />
+        <title></title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#252525"
+        />
+        <meta name="msapplication-TileColor" content="#252525" />
+        <meta name="theme-color" content="#252525" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
